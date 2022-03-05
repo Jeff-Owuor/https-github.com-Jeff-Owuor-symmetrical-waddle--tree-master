9755c2f3e0319c394ea364dcd8f199f61e3970d4
Jeff <Xavierjeff451@gmail.com>
2022-03-05 16:35:43 +0300

    Grabbed the form elements in the dom

---
+++ app.js
@@ -0,0 +1,8 @@
+const centuryOfBirth=document.getElementById("century");
+const yearOfBirth=document.getElementById("year");
+const monthOfBirth=document.getElementById("month");
+const birthday=document.getElementById("day");
+let century=centuryOfBirth.value;
+let year=yearOfBirth.value;
+let month=monthOfBirth.value;
+let day=birthday.value;
